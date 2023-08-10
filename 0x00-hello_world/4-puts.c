1f76ed1389f8944fa28581047140c81232c9ee02
Shiraz880 <shirazalsheekh@gmail.com>
2023-08-10 16:40:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: A C program that prnits with put fuction
+ * Description: C program that prnits with put fuction
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"):
