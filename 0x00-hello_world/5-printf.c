6dc3759fc9bcc3cb88bfd348d427a8acbd6c871d
Shiraz880 <shirazalsheekh@gmail.com>
2023-08-10 17:24:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program that print with printf function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
