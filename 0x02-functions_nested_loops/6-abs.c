fbe2577cfbe6c411caf7e93d8cbca2d7c834cd9e
Shiraz880 <shirazalsheekh@gmail.com>
2023-08-16 17:28:53 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absoloute value of 
+ * integar
+ *
+ * @n: take an integar type input for function
+ *
+ * Return: always 0
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) + n;
+	return (n);
+}
