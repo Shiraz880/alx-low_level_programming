ea410a7414bcefa4039bbcbd22324fe8fe21d7cd
Shiraz880 <shirazalsheekh@gmail.com>
2023-08-16 17:19:42 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - print + if n is greater than zero, 0 if
  * n is zero and - if n is lesser than zero
  *
- * @n: take integer type input for function.
+ * @c: take integer type input for function.
  *
  * Return: 1 if +, 0 if 0 and -1 if -
  */
